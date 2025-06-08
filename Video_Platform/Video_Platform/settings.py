c525e9882b75855da7b7632beb07afff5c748b4b
Samarthgit-code <samarth192025@gmail.com>
2025-06-08 23:04:43 +0530

    Update settings.py

--- Video_Platform/Video_Platform/settings.py
+++ Video_Platform/Video_Platform/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+import os
+import dj_database_url
+
+
+Key: DATABASE_URL
+Value: postgresql://vidmora_f2z4_user:h8bM4VKzDixMJhnshbKt27av6dXmr6hD@dpg-d12sdqbipnbc73bbftsg-a/vidmora_f2z4
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,11 +96,9 @@ WSGI_APPLICATION = 'Video_Platform.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
